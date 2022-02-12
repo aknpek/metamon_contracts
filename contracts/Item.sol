435cdc88ceac2f1784e8411b75e0389f604a4bc2
aknpek <32802120+aknpek@users.noreply.github.com>
2022-02-12 18:04:05 +0100

    Item token developments

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -6,31 +6,39 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract Item is ERC721 {
     using Strings for uint256;
 
-    string private passWord = "MADECHANGE";
+    address payable public owner;
 
     uint[7] private itemBurnable = [1, 0, 0, 0, 0, 0, 0];
     uint8[7] private itemTypes = [1, 2, 3, 4, 5, 6, 7];
-    uint8[7] private maxOwnable = [1, 5, 5, 5, 5, 5, 5];
-    uint32[7] private itemSupplies = [1000, 2000, 3000, 4000, 5000, 2000, 3000];
+    uint8[7] private maxOwnable = [10, 1, 1, 1, 1, 1, 1];
+    uint32[7] private itemSupplies = [2500, 2500, 1000, 1000, 1000, 1000, 1000];
 
-    uint256 public itemTotalSupply;
-    uint256[7] private itemFloor = [0.2 ether, 0.3 ether, 0.4 ether, 0.2 ether, 0.2 ether, 0.3 ether, 0.1 ether];
-
-    address payable public owner;
+    uint256[7] private itemFloor = [0.12 ether, 0.25 ether, 0.04 ether, 0.04 ether, 0.04 ether, 0.04 ether, 0.04 ether];
+    uint256 private itemTokenIds;
+    uint256 public itemTotalSupply = 10000;
 
     string private itemBaseURI;
     string private baseURI;
+    string private passWord = "MADECHANGE";
 
     mapping(uint256 => uint8) public tokenItemTypes;
-
+    mapping(address => uint8[]) public tokenOwner;
+    mapping(address => mapping(uint8 => uint256[])) public tokenOwners; // if the owner owns specific tokens in array 
+    
+    ///////////////////////////////////////////////////////////////////////////
+    // Events
+    ///////////////////////////////////////////////////////////////////////////
     event RecievedEth(address _sender, uint256 _value);
     event ItemMinted(address _reciever, uint256 _tokenId);
+    event burnItem(address _burner, uint256 _tokenId);
 
     constructor() payable ERC721("Metamon Item Collection", "NFT"){
         owner = payable(msg.sender);
     }
 
+    ///////////////////////////////////////////////////////////////////////////
     // Pre-Function Conditions
+    ///////////////////////////////////////////////////////////////////////////
     modifier onlyOwner(address _sender) {
         require(_sender == owner, "Not a owner call!");
         _;
@@ -42,11 +50,14 @@ contract Item is ERC721 {
     }
 
     modifier passCheck(string memory _passCode){
+        // This modifier limits the access into mintFunction
         require(keccak256(abi.encodePacked(_passCode)) == keccak256(abi.encodePacked(passWord)), "Token not match!");
         _;
     }
 
+    ///////////////////////////////////////////////////////////////////////////
     // State Changes
+    ///////////////////////////////////////////////////////////////////////////
     function changeFloorPrice(
         uint256 _new_price,
         uint8 _itemType
@@ -55,9 +66,30 @@ contract Item is ERC721 {
     }
 
     function getFloorPrice(
-        uint256 _itemType
-    ) external itemType(_itemType) view returns(uint256){
+        uint8 _itemType
+    ) itemType(_itemType) public view returns(uint256){
         return itemFloor[_itemType];
+    }
+
+    function getSupplyLeft(
+        uint8 _itemType
+    ) itemType(_itemType) public view returns(uint256){
+        return itemSupplies[_itemType]; 
+    }
+
+    function mintableLeft(
+        uint256 _quantity,
+        uint256 _itemSupplyLeft
+    ) internal pure returns(uint256){
+        if (_quantity < _itemSupplyLeft){
+            return _quantity;
+        } else {
+            if (_itemSupplyLeft == 0){
+                return 0;
+            } else{
+                return _itemSupplyLeft;
+            }
+        }
     }
 
     function burn(uint256 _tokenId) private {
@@ -65,12 +97,25 @@ contract Item is ERC721 {
     }
 
     function mintSale(
-        address _recipeint,
         string memory _passCode,
+        address _recipeint,
         uint8 _itemType,
         uint256 _quantity
     ) external passCheck(_passCode) payable {
+        // get first token conditions
+        // check the how many has been owned by the user
+        // check how many has been asked
+        uint256 _itemSupplyLeft = getSupplyLeft(_itemType);
+        uint256 _itemFloor = getFloorPrice(_itemType);
+        uint256 _mintableLeft = mintableLeft(_quantity, _itemSupplyLeft);
 
-        
+        require(_mintable * _itemFloor == msg.value, "Not exact coin send!");
+
+
+
+    }
+
+    function totalItemTypes() public view returns(uint256){
+        return itemTypes.length; 
     }
 }
