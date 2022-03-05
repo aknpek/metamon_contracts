60813ec01e26536c90bfe3db9866a5219dd93aa9
aknpek <32802120+aknpek@users.noreply.github.com>
2022-03-05 16:34:22 +0100

    metamon and item mint test

--- test/MetamonContract3.test.js
+++ test/MetamonContract3.test.js
@@ -2,25 +2,43 @@ const readYaml = require("./readYaml.js");
 const yaml_data = readYaml("./test/testCases.yml");
 const Web3 = require("web3");
 
+const item_contract_address = yaml_data["ItemContract"]["contractName"];
 const metamon_contract_name = yaml_data["MetamonContract"]["contractName"];
+
+const ItemContract = artifacts.require(item_contract_address);
 const MetamonContract = artifacts.require(metamon_contract_name);
-const recipient = yaml_data["MetamonContract"]["contractName"];
-const 
+
+const recipient = yaml_data["MetamonContract"]["testCase4"]["recipient"];
+const quantity_mint_metamon = yaml_data["MetamonContract"]["testCase4"]["quantityMintMetamon"];
+const mint_dex_id = yaml_data["MetamonContract"]["testCase4"]["mintDexId"];
+
+const quantity_mint_item = yaml_data["MetamonContract"]["testCase4"]["quantityMintItem"];
+const mint_item_type = yaml_data["MetamonContract"]["testCase4"]["mintItemType"];
+const pass_code = yaml_data["MetamonContract"]["testCase4"]["passCode"];
 
 
 contract("Metamon", () => {
+    let itemContract = null;
     let metamonContract = null;
+
     before(async () => {
+        itemContract = await ItemContract.deployed();
         metamonContract = await MetamonContract.deployed();
         contractOwner = await metamonContract.owner.call();
     });
 
-    it("check evalutionItemBurn", async () => {
-        metamonContract.evalutionItemBurn(
+    
+    it("Check Item Mint", async() => {
+        await itemContract.mintSale(
+            pass_code,
             recipient,
-            sendTokenId,
-            sendDexId
-        )
+            mint_item_type,
+            quantity_mint_item
+        );
+        
+        const howManyItemMinted = await itemContract.balanceOf(recipient, {from: recipient});
+        assert(howManyItemMinted.toNumber() == quantity_mint_item);
     });
+
 
 })
