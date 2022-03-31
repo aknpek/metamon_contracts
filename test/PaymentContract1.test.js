d3b15682f8ac7e4e1c3b10c700d710b3fd5b7668
aipek <aipek@deloitte.nl>
2022-03-31 19:16:50 +0200

    payment test methods adding

--- test/PaymentContract1.test.js
+++ test/PaymentContract1.test.js
@@ -1,6 +1,7 @@
 const readYaml = require("./readYaml.js");
 const yaml_data = readYaml("./test/testCases.yml");
 const Web3 = require("web3");
+
 const payment_contract_name = yaml_data["PaymentContract"]["contractName"];
 const PaymentContract = artifacts.require(payment_contract_name);
 
@@ -13,10 +14,12 @@ const percantage2 = yaml_data["PaymentContract"]["testCase1"]["percantage2"];
 const withDrawer3 = yaml_data["PaymentContract"]["testCase1"]["withDrawer3"];
 const percantage3 = yaml_data["PaymentContract"]["testCase1"]["percantage3"];
 
-const phaseType1 = yaml_data["PaymentContract"]["testCase1"]["phaseType"];
+const phaseType1 = yaml_data["ItemContract"]["contractAddress"];
+const phaseType2 = yaml_data["MetamonContract"]["contractAddress"];
 
 contract("Payment", () => {
   let paymentContract = null;
+
   before(async () => {
     paymentContract = await PaymentContract.deployed();
     contractOwner = await paymentContract.owner.call();
@@ -24,5 +27,35 @@ contract("Payment", () => {
 
   it("Handle add first withdrawer", async () => {
     await paymentContract.addWithdrawer(phaseType1, withDrawer1, percantage1);
+
+    const phaseOwners = await paymentContract.phaseTypes.call(
+      phaseType1,
+      withDrawer1
+    );
+
+    assert.equal(phaseOwners.isExist, true);
+    assert.equal(phaseOwners.percantage, percantage1);
+    assert.equal(phaseOwners.payableAmount.toNumber(), 0);
+  });
+
+  it("Handle add multiple withdrawer", async () => {
+    // TODO: for loop needs to be refactored
+    await paymentContract.addWithdrawer(phaseType1, withDrawer1, percantage1);
+    await paymentContract.addWithdrawer(phaseType1, withDrawer2, percantage2);
+
+    await paymentContract.removeWithdrawer(phaseType1, withDrawer1, {
+      from: contractOwner,
+    });
+  });
+
+  it("Handle to remove non-exist withdrawer", async () => {
+    try {
+      await paymentContract.removeWithdrawer(phaseType1, withDrawer3);
+      assert(false);
+      return;
+    } catch {
+      assert(true);
+      return;
+    }
   });
 });
