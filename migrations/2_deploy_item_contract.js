c9ce9bb6822157fff94abbf9393ea9b791d1efa8
aipek <aipek@deloitte.nl>
2022-04-16 14:52:05 +0200

    node modules removed

--- migrations/2_deploy_item_contract.js
+++
@@ -1,5 +0,0 @@
-const Contract = artifacts.require("Item");
-
-module.exports = function (deployer) {
-  deployer.deploy(Contract);
-};
--- migrations/4_deploy_payment_contract.js
+++
@@ -1,5 +0,0 @@
-const Contract = artifacts.require("Payment");
-
-module.exports = function (deployer) {
-  deployer.deploy(Contract);
-};
