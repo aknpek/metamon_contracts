823c2a417d8bf42734e950faa992a5a4c203a706
aipek <aipek@deloitte.nl>
2022-04-26 01:42:02 +0200

    erc1155 added

--- contracts/MetaTrainer.sol
+++ contracts/MetaTrainer.sol
@@ -1,67 +1,75 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract MetaTrainer is ERC721, Ownable {
-    
-    //Pre-reveal IPFS link
-    string private baseURI = "";
+contract MetaTrainer is ERC1155, Ownable {
+    using Strings for uint256;
 
-    constructor() ERC721("MiniMetamon Trainer", "MiniMetamon-TR") {}
+    uint8 public maxMintPerOwner = 1;
+    uint256 public mintPrice = 0 ether;
+    uint256 public trainerId;
+    uint256 public currentTrainerSupply;
 
-    uint256 public currentSupply;
+    string public baseUri;
 
-    event TrainerMint(uint256 _tokenId, address _reciever);
+    event ReceiveEth(address _sender, uint256 _amount);
+    event TrainerCreated(address _minter, uint256 _tokenId);
 
-    function mintTrainer() public {
-        uint256 supply = currentSupply;
+    constructor() payable ERC1155("Meta Trainer NFT") {}
 
-        _mint(msg.sender, (supply + 1));
+    ///////////////////////////////////////////////////////////////////////////
+    // Mint Functions
+    ///////////////////////////////////////////////////////////////////////////
+    function mintTrainer(
+        address minter,
+        uint256 amount,
+        bytes memory data
+    ) public payable {
+        require(
+            balanceOf(minter, trainerId) + amount <= maxMintPerOwner,
+            "Max Trainer Mint 1!"
+        );
+        require(msg.value >= amount * mintPrice, "Not Enough Funds!");
+        require(amount >= currentTrainerSupply, "Not Enough Supply!");
 
-        currentSupply += 1;
-
-        emit TrainerMint(currentSupply, msg.sender);
+        _mint(minter, trainerId, amount, data);
+        emit TrainerCreated(minter, trainerId);
     }
 
-    function tokensOfOwner(
-        address _owner,
-        uint256 startId,
-        uint256 endId
-    ) external view returns (uint256[] memory) {
-        uint256 tokenCount = balanceOf(_owner);
-        if (tokenCount == 0) {
-            return new uint256[](0);
-        } else {
-            uint256[] memory result = new uint256[](tokenCount);
-            uint256 index = 0;
-            for (uint256 tokenId = startId; tokenId < endId; tokenId++) {
-                if (index == tokenCount) break;
-
-                if (ownerOf(tokenId) == _owner) {
-                    result[index] = tokenId;
-                    index++;
-                }
-            }
-
-            return result;
-        }
-    }
-
-    function walletOfOwner(address _owner) external view returns (uint256[] memory)
-    {
-        return this.tokensOfOwner(_owner, 0, currentSupply);
-    }
-
-    //TODO: Need to discuss this with Nick
     function withdraw() external onlyOwner {
         (bool owner, ) = address(this).call{value: address(this).balance}("");
         require(owner);
     }
 
-    function setBaseURI(string memory _uri) external onlyOwner {
-        baseURI = _uri;
+    function setTokenUri(string memory uri) public virtual override onlyOwner {
+        baseUri = uri;
+    }
+
+    // function uri(uint256 tokenId) public view override returns (string memory) {
+    //     return
+    //         bytes(baseUri).length > 0
+    //             ? string(abi.encodePacked(baseUri, tokenId.toString(), ".json"))
+    //             : "";
+    // }
+
+    function uri(uint256 _tokenid)
+        public
+        pure
+        override
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    "https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/",
+                    Strings.toString(_tokenid),
+                    ".json"
+                )
+            );
     }
 }
