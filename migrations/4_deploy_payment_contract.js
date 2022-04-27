db15e7d632727801d9d6b6ee5ef1785628c7f8bc
aipek <aipek@deloitte.nl>
2022-04-27 10:56:08 +0200

    meta trainer contract fix + meta trainer contract receive eth witdhdraw

--- migrations/4_deploy_payment_contract.js
+++ migrations/4_deploy_payment_contract.js
@@ -6,5 +6,5 @@ const contractConcensus = yaml_data["PaymentContract"]["contractConcensus"];
 const Contract = artifacts.require("Payment");
 
 module.exports = function (deployer) {
-  deployer.deploy(Contract, { value: contractConcensus });
+  deployer.deploy(Contract, (concensus = contractConcensus));
 };
