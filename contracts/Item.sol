ea8f6037b0f183c512f09a0332142eaabfc5456f
Westycoot <ben.westcott@gmail.com>
2022-07-06 16:57:40 +0100

    Updated item contract to new logic  and structure.

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -12,63 +12,45 @@ contract Item is ERC1155Supply, Ownable, ReentrancyGuard {
     using Strings for string;
 
     //Set contract name and symbol
-    string public name = "MiniMetamon Item";
-    string public symbol = "MiniMetamon-Item";
+    string public name;
+    string public symbol;
 
     address payable public paymentContractAddress;
+    uint8 public currentPhase;
+    
+    struct ItemTypeInfo {
+        uint256 itemPrice;
+        uint256 maxMintable;
+        uint256 itemSupply;
+        bool itemBurnable;
+        string uri;
+        bool valid;
+    }
+   
+    //Need to add wallet limits per phase and max ownable
 
-    uint8 private TEAR_OF_THE_GODDESS = 1;
-    uint8 private LUCKY_TOTEM = 2;
-    uint8 private SPIRIT_OF_FIRE = 3;
-    uint8 private SPIRIT_OF_WATER = 4;
-    uint8 private SPIRIT_OF_EARTH = 5;
-    uint8 private SPIRIT_OF_ELECTRICTY = 6;
-    uint8 private ASTRAL_SPIRIT = 7;
+    mapping(uint256 => ItemTypeInfo) public itemTypes;
+    mapping(address => mapping(uint256 => uint256)) public itemsMinted; // Represents owner's different type of tokens' ids
 
-    uint8[7] public itemBurnable = [1, 0, 0, 0, 0, 0, 0];
-    uint8[7] public itemTypes = [TEAR_OF_THE_GODDESS,
-                                 LUCKY_TOTEM,
-                                 SPIRIT_OF_FIRE,
-                                 SPIRIT_OF_WATER,
-                                 SPIRIT_OF_EARTH,
-                                 SPIRIT_OF_ELECTRICTY,
-                                 ASTRAL_SPIRIT];
-    uint8[7] public maxOwnable = [10, 1, 1, 1, 1, 1, 1];
-    uint32[7] public itemSupplies = [2500, 2500, 1000, 1000, 1000, 1000, 1000];
-
-    uint256[7] private itemFloor = [
-        0.12 ether,
-        0.25 ether,
-        0.04 ether,
-        0.04 ether,
-        0.04 ether,
-        0.04 ether,
-        0.04 ether
-    ];
-    uint256 private tokenIds;
-    uint256 public itemTotalSupply = 10000;
-
-    string private itemBaseURI;
-    string private baseURI;
-    string private passWord = "MADECHANGE";
-
-    mapping (uint256 => string) private uris;
-
-    mapping(uint256 => uint8) public tokenItemTypes;
-    mapping(address => uint256[]) public tokenOwner; // Represents owner's tokens *tokenIds
-    mapping(address => mapping(uint8 => uint256[])) public tokenOwners; // Represents owner's different type of tokens' ids
+    uint256 numberOfItemTypes;
 
     ///////////////////////////////////////////////////////////////////////////
     // Events
     ///////////////////////////////////////////////////////////////////////////
     event ReceivedEth(address _sender, uint256 _value);
-    event ItemMinted(address _reciever, uint256 _tokenId);
-    event BurnItem(address _burner, uint256 _tokenId);
+    event ItemsMinted(address _receiver, uint256[] _tokenIds, uint256[] _quantity);
+    event ItemsBurned(address _burner, uint256[] _tokenIds, uint256[] _quantity);
 
     ///////////////////////////////////////////////////////////////////////////
     // Cons
     ///////////////////////////////////////////////////////////////////////////
-    constructor() payable ERC1155("https://gateway.pinata.cloud/ipfs/INSERT_IPFS_HASH_HERE/{id}.json") {}
+    constructor() 
+        payable 
+        ERC1155("https://gateway.pinata.cloud/ipfs/INSERT_IPFS_HASH_HERE/{id}.json") 
+        {
+             name = "MiniMetamon Item";
+             symbol = "MiniMetamon-Item";
+        }
 
     fallback() external payable {}
 
@@ -79,78 +61,99 @@ contract Item is ERC1155Supply, Ownable, ReentrancyGuard {
     ///////////////////////////////////////////////////////////////////////////
     // Pre-Function Conditions
     ///////////////////////////////////////////////////////////////////////////
-    modifier itemType(uint256 _itemType) {
-        require(1 <= _itemType && _itemType <= 7, "Item Type out of scope!");
+     modifier itemTypeCheck(uint256 _itemType) {
+        require(itemTypes[_itemType].valid, "Item Type out of scope!");
         _;
     }
 
-    modifier passCheck(string memory _passCode) {
-        // This modifier limits the access into mintFunction
+    modifier itemTypesCheck(uint256[] memory _itemTypes) {
+        for (uint i = 0; i < _itemTypes.length; i++) {
+            require(
+                itemTypes[_itemTypes[i]].valid,
+                "Item Type is out of scope!"
+            );
+        }
+        _;
+    }
+
+    modifier maxMintableCheck(uint256 _itemType, uint256 _quantity) {
         require(
-            keccak256(abi.encodePacked(_passCode)) ==
-                keccak256(abi.encodePacked(passWord)),
-            "Token not match!"
+            itemsMinted[msg.sender][_itemType] + _quantity <=
+                itemTypes[_itemType].maxMintable,
+            "User is trying to mint more than allocated."
+        );
+        require(
+            totalSupply(_itemType) + _quantity <=
+                itemTypes[_itemType].itemSupply,
+            "User is trying to mint more than total supply."
         );
         _;
     }
-
     ///////////////////////////////////////////////////////////////////////////
     // Get/Set State Changes
     ///////////////////////////////////////////////////////////////////////////
-    function changeFloorPrice(uint256 _new_price, uint8 _itemType)
+     function getItemPrice(uint256 _itemType)
+        public
+        view
+        itemTypeCheck(_itemType)
+        returns (uint256)
+    {
+        return itemTypes[_itemType].itemPrice;
+    }
+
+    function setMaxMintable(uint256 _maxMintable, uint256 _itemType)
         external
         onlyOwner
-        itemType(_itemType)
+        itemTypeCheck(_itemType)
     {
-        itemFloor[_itemType - 1] = _new_price;
+        itemTypes[_itemType].maxMintable = _maxMintable;
     }
 
-    function getFloorPrice(uint8 _itemType)
+    function getMaxMintable(uint256 _itemType)
         public
         view
-        itemType(_itemType)
+        itemTypeCheck(_itemType)
         returns (uint256)
     {
-        return itemFloor[_itemType - 1];
+        return itemTypes[_itemType].maxMintable;
     }
 
-    function getSupplyLeft(uint8 _itemType)
+    function setItemSupply(uint256 _itemSupply, uint256 _itemType)
+        external
+        onlyOwner
+        itemTypeCheck(_itemType)
+    {
+        itemTypes[_itemType].itemSupply = _itemSupply;
+    }
+
+    function getItemSupply(uint256 _itemType)
         public
         view
-        itemType(_itemType)
+        itemTypeCheck(_itemType)
         returns (uint256)
     {
-        return itemSupplies[_itemType - 1];
+        return itemTypes[_itemType].itemSupply;
     }
 
-    function specificItemOwnership(address _owner, uint8 _itemType)
+    function setItemBurnable(bool _burnable, uint256 _itemType)
+        external
+        onlyOwner
+        itemTypeCheck(_itemType)
+    {
+        itemTypes[_itemType].itemBurnable = _burnable;
+    }
+
+    function getItemBurnable(uint256 _itemType)
         public
         view
-        returns (uint256)
+        itemTypeCheck(_itemType)
+        returns (bool)
     {
-        // check whether owner owns specific ITEM token
-        uint256 total_ownership = tokenOwners[_owner][_itemType].length;
-        return total_ownership;
-    }
-
-    function mintableLeft(uint256 _quantity, uint256 _itemSupplyLeft)
-        internal
-        pure
-        returns (uint256)
-    {
-        if (_quantity <= _itemSupplyLeft) {
-            return _quantity;
-        } else {
-            if (_itemSupplyLeft == 0) {
-                revert("Item supply not enough!");
-            } else {
-                return _itemSupplyLeft;
-            }
-        }
+        return itemTypes[_itemType].itemBurnable;
     }
 
     function totalItemTypes() public view returns (uint256) {
-        return itemTypes.length;
+        return numberOfItemTypes;
     }
 
     function setPayableAddress(address payable _paymentContractAddress)
@@ -163,91 +166,59 @@ contract Item is ERC1155Supply, Ownable, ReentrancyGuard {
     ///////////////////////////////////////////////////////////////////////////
     // Burn Tokens
     ///////////////////////////////////////////////////////////////////////////
-    function _deleteOwnerToken(address _burner, uint256 _tokenId)
-        internal
-        onlyOwner
-        returns (bool)
+
+    function burnItem(uint256[] memory itemTypes, uint256[] memory quantities)
+        external
     {
-        // TODO: redundant function, change it with ownerOf() -> when the item will be burned it will be automatically removed
-        uint256[] memory tokens = tokenOwner[_burner];
-        for (uint256 i = 0; i < tokens.length; i++) {
-            if (tokens[i] == _tokenId) {
-                delete tokenOwner[_burner][i];
-                return true;
-            }
-        }
-        return false;
+        _burnBatch(msg.sender, itemTypes, quantities);
     }
 
-    // TODO: Only tokens owned by the holder can be burned by the holder
-    function burn(address _burner, uint256 _tokenId) public {
-        uint8 _tokenType = tokenItemTypes[_tokenId];
-        require(itemBurnable[_tokenType - 1] == 1, "Item not burnable!");
-
-        if (_deleteOwnerToken(_burner, _tokenId)) {
-            _burn(_burner, _tokenId, 1);
-            emit BurnItem(_burner, _tokenId);
-        } else {
-            revert("Could not burn the token!");
-        }
-    }
     ///////////////////////////////////////////////////////////////////////////
     // Mint Tokens
     ///////////////////////////////////////////////////////////////////////////
     function mintSale(
-        string memory _passCode,
-        address _recipient,
-        uint8 _itemType,
-        uint256 _quantity
-    ) external payable passCheck(_passCode) nonReentrant {
+        uint256[] memory _itemTypes,
+        uint256[] memory _quantity
+    ) external payable itemTypesCheck(_itemTypes) nonReentrant {
+        uint256 totalMintCost;
 
-        uint256 _itemSupplyLeft = getSupplyLeft(_itemType);
-
-        uint256 _totalOwned = specificItemOwnership(_recipient, _itemType);
-        uint256 _maxOwnable = maxOwnable[_itemType - 1];
-        require(
-            _totalOwned + _quantity <= _maxOwnable,
-            "Max ownable quantity reached!"
-        );
-
-        uint256 _itemFloor = getFloorPrice(_itemType);
-
-        uint256 _maxMintable = mintableLeft(_quantity, _itemSupplyLeft);
-        if (msg.sender != owner()) {
+        for (uint i = 0; i < _itemTypes.length; i++) {
             require(
-                _maxMintable * _itemFloor <= msg.value,
-                "Not exact coin send!"
+                itemsMinted[msg.sender][_itemTypes[i]] + _quantity[i] <=
+                    itemTypes[_itemTypes[i]].maxMintable,
+                "User is trying to mint more than allocated."
             );
+
+            require(
+                totalSupply(_itemTypes[i]) + _quantity[i] <=
+                    itemTypes[_itemTypes[i]].itemSupply,
+                "User is trying to mint more than total supply."
+            );
+
+            totalMintCost += itemTypes[_itemTypes[i]].itemPrice * _quantity[i];
         }
 
-        uint256 j = tokenIds;
+        require(msg.value == totalMintCost, "Not enough ETH to mint!");
 
-        for (uint256 i = 0; i < _quantity; i++) {
-            j++;
-            _mint(_recipient, _itemType, 1, "");
-            tokenOwners[_recipient][_itemType].push(j);
-            tokenOwner[_recipient].push(j);
-            tokenItemTypes[j] = _itemType;
-            itemSupplies[_itemType - 1] = itemSupplies[_itemType - 1] - 1;
-            emit ItemMinted(_recipient, j);
+        for (uint i = 0; i < _itemTypes.length; i++) {
+            itemsMinted[msg.sender][_itemTypes[i]] += _quantity[i];
         }
-        tokenIds = j;
+
+        _mintBatch(msg.sender, _itemTypes, _quantity, "");
+        emit ItemsMinted(msg.sender, _itemTypes, _quantity);
     }
-
     ///////////////////////////////////////////////////////////////////////////
     // Backend URIs
     ///////////////////////////////////////////////////////////////////////////
-    function uri(uint256 tokenId) override public view returns (string memory){
-        return(uris[tokenId]);
+    function uri(uint256 _itemType) public view override returns (string memory) {
+        return (itemTypes[_itemType].uri);
     }
 
-    function setTokenUri(uint256 tokenId, string memory uri) public onlyOwner {
-        uris[tokenId] = uri;
-    }
-
-    function setURI(string memory newuri) external onlyOwner 
+    function setTokenUri(uint256 _itemType, string memory newUri)
+        external
+        onlyOwner
     {
-        _setURI(newuri);
+        itemTypes[_itemType].uri = newUri;
     }
 
     ///////////////////////////////////////////////////////////////////////////
